297b6e61d0fc6d73aea8d32b4a7d42345a04cd37
chaddziadosz <chaddziadosz87@gmail.com>
2021-03-27 10:27:04 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,17 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
         event.preventDefault();
-        
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+        var taskNameInput = document.querySelector("input[name='task-name']").value;
+        console.dir(taskNameInput);
+        var taskTypeInput = document.querySelector("select[name='task-type']").value;
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+listItemEl.appendChild(taskInfoEl);
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+tasksToDoEl.appendChild(listItemEl);
   };
-  formEl.addEventListener("submit", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler); console.dir(listItemEl);
+  
 
